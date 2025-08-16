d014106f8a8436f3e2fd5bc5dc98e69c1559906b
Anhar12 <anharkhoirun@gmail.com>
2025-08-17 06:46:29 +0800

    akhirnya

--- app.py
+++ app.py
@@ -48,15 +48,15 @@ CLASS_INFO = {
 # ==== Fungsi preprocessing ====
 def preprocess_image(img):
     # CLAHE untuk tingkatkan kontras objek
-    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    l_clahe = clahe.apply(l)
-    lab_clahe = cv2.merge((l_clahe, a, b))
-    img_clahe = cv2.cvtColor(lab_clahe, cv2.COLOR_LAB2BGR)
+    # lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    # l, a, b = cv2.split(lab)
+    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    # l_clahe = clahe.apply(l)
+    # lab_clahe = cv2.merge((l_clahe, a, b))
+    # img_clahe = cv2.cvtColor(lab_clahe, cv2.COLOR_LAB2BGR)
 
     # Canny edge
-    edges = cv2.Canny(img_clahe, threshold1=50, threshold2=180)
+    edges = cv2.Canny(img, threshold1=50, threshold2=180)
 
     # Resize sesuai input model CNN
     img_resized = cv2.resize(edges, (244, 244))
@@ -76,7 +76,6 @@ def preprocess_image(img):
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
-        
         if "file" not in request.files:
             return jsonify({"error": "Missing file"}), 400
 
