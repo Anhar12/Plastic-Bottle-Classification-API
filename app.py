f72b59cbf6d6780c62bfffb5f1f46035e42b7318
Anhar12 <anharkhoirun@gmail.com>
2026-01-27 11:41:19 +0800

    add CLAHE

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ CLASS_INFO = {
     3: {"code": "D", "brand": "Squades", "size": "1500ml", "weight": 30},
     4: {"code": "E", "brand": "Crystaline", "size": "1500ml", "weight": 30},
     5: {"code": "F", "brand": "Aqua", "size": "1500ml", "weight": 30},
-    6: {"code": "G", "brand": "Prima", "size": "600ml", "weight": 16},
+    6: {"code": "G", "brand": "Ron88", "size": "600ml", "weight": 16},
     7: {"code": "H", "brand": "Le Minerale", "size": "600ml", "weight": 17},
     8: {"code": "I", "brand": "Aqua", "size": "600ml", "weight": 17},
     9: {"code": "J", "brand": "Crystaline", "size": "600ml", "weight": 17},
@@ -78,60 +78,12 @@ def get_db():
     )
     return conn
 
-bg_color = np.array([132,132,132])
-tolerance = np.array([15, 15, 15])
-
-def compress_image(img_bgr, max_size=800):
-    h, w = img_bgr.shape[:2]
-    scale = max(h, w) / max_size
-    if scale > 1:  # resize hanya jika lebih besar dari max_size
-        new_w, new_h = int(w / scale), int(h / scale)
-        img_bgr = cv2.resize(img_bgr, (new_w, new_h), interpolation=cv2.INTER_AREA)
-    return img_bgr
-
-def preprocess_image_bgr(img_bgr: np.ndarray):
-    img_bgr = compress_image(img_bgr, max_size=800)
-    
-    # CLAHE
-    lab = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=1.25, tileGridSize=(16, 16))
-    l_clahe = clahe.apply(l)
-    lab_clahe = cv2.merge((l_clahe, a, b))
-    img_clahe = cv2.cvtColor(lab_clahe, cv2.COLOR_LAB2BGR)
-
-    # Buat mask background
-    lower = np.clip(bg_color - tolerance, 0, 255)
-    upper = np.clip(bg_color + tolerance, 0, 255)
-    mask_bg = cv2.inRange(img_clahe, lower, upper)
-
-    # Perbesar mask sedikit biar nutup tepi objek
-    mask_bg_dilated = cv2.dilate(mask_bg, np.ones((5,5), np.uint8))
-
-    # Blur hanya di bagian tepi background
-    blurred = img_clahe.copy()
-    blurred_bg = cv2.GaussianBlur(img_clahe, (3,3), 0)
-    blurred[mask_bg_dilated > 0] = blurred_bg[mask_bg_dilated > 0]
-
-    # Canny edge
-    edges = cv2.Canny(blurred, threshold1=50, threshold2=180)
-
-    # Hapus edge yang masih di background murni
-    edges[mask_bg > 0] = 0
-
-    # Resize -> (224, 224, 1)
-    img_resized = cv2.resize(edges, (224, 224))
-    img_normalized = img_resized.astype("float32") / 255.0
-    img_final = np.expand_dims(img_normalized, axis=-1)   # (224,224,1)
-    img_final = np.expand_dims(img_final, axis=0)         # (1,224,224,1)
-    return img_final
-
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
         if "file" not in request.files:
             return jsonify({"error": "Missing file"}), 400
-        
+
         file = request.files["file"]
         filename = datetime.now().strftime("%Y%m%d_%H%M%S") + "_" + str(uuid.uuid4()) + ".jpg"
         filepath = os.path.join(UPLOAD_FOLDER, filename)
@@ -141,22 +93,30 @@ def predict():
         img_bgr = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
         if img_bgr is None:
             return jsonify({"error": "Invalid image"}), 400
-        
+
         _, pred_fn = get_model()
-        
+
+        img_lab = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(img_lab)
+
+        clahe = cv2.createCLAHE(clipLimit=4, tileGridSize=(8, 8))
+        l = clahe.apply(l)
+
+        img_lab = cv2.merge((l, a, b))
+        img_bgr = cv2.cvtColor(img_lab, cv2.COLOR_LAB2BGR)
+
         img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-        img_resized = cv2.resize(img_rgb, (224, 224))
-        img_normalized = img_resized.astype("float32") / 255.0
+        img_rgb = cv2.resize(img_rgb, (224, 224), interpolation=cv2.INTER_AREA)
+        img_rgb = img_rgb.astype("float32") / 255.0
 
-        x = np.expand_dims(img_normalized, axis=0)
+        x = np.expand_dims(img_rgb, axis=0)
 
-        preds = pred_fn(tf.convert_to_tensor(x))
-        preds = preds.numpy()
+        preds = pred_fn(tf.convert_to_tensor(x)).numpy()
         class_idx = int(np.argmax(preds, axis=1)[0])
         confidence = float(np.max(preds)) * 100
 
         info = CLASS_INFO[class_idx]
-        
+
         return jsonify({
             "code": info["code"],
             "brand": info["brand"],
@@ -164,6 +124,7 @@ def predict():
             "weight": info["weight"],
             "confidence": round(confidence, 2)
         })
+
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
